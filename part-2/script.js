4f0c7473bfb23b16971cf73b4aecbd57b35432af
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-04-28 08:07:36 -0500

    done

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,35 @@
 // Do your work in this file.
+
+// var view = $("<div>");
+
+// view.addClass('view');
+
+// $("body").append(view);
+
+ 
+
+for (var i = 0; i < 197; i++) {
+
+
+  var box = $("<div>");
+
+  if (i%2 == 0) {
+
+    box.addClass("box type-1");
+
+  } else {
+
+    box.addClass("box type-2");
+
+  }
+
+  $("body").append(box);
+
+}
+
+$(".box").click(function() {
+
+  $(this).toggleClass("clicked");
+
+  }
+)
